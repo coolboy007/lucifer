410ed5f6a3a064a53ffa8838124fc8118a5effca
coolboy007 <61577388+coolboy007@users.noreply.github.com>
2022-02-25 19:50:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/coolboy007/lucifer.git  /lucifer
-cd /lucifer
+git clone -b master https://github.com/coolboy007/lucifersd.git  /lucifersd
+cd /lucifersd
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
